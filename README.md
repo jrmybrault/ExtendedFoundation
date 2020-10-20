f850112a8e540421e4f62481f6f4b92cb4d140a8
Jérémy Brault <jeremy.brault@blablacar.com>
2020-10-20 08:00:42 +0200

    Add SetupOperator and update README

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ExtendedFoundation
 
  `ExtendedFoundation` is a collection of new APIs and extensions for the Apple `Foundation` library. 
+
+## TODO
+
+- Setup SwiftLint
+- Check coverage and improve it if necessary
---
+++ Sources/ExtendedFoundation/Operators/SetupOperator.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jérémy Brault on 20/10/2020.
+//
+
+import Foundation
+
+prefix operator =>
+infix operator => : MultiplicationPrecedence
+
+@discardableResult
+public prefix func => <T>(setup: () -> T) -> T {
+    setup()
+}
+
+@discardableResult
+public func => <T>(object: T, setup: (inout T) -> Void) -> T {
+    var object = object
+    setup(&object)
+    return object
+}
